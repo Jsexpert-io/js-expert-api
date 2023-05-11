faa1b5aab478052002210f8b390058ffdb4975a9
bhavanad05 <bhavana.nd05@gmail.com>
2023-05-11 16:04:38 +0530

    changes for dashboad screen

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -8,15 +8,15 @@ import { decode, sign, verify } from 'jsonwebtoken';
 @Injectable()
 export class AuthenticationService {
   async verifyToken(token: any) {
-   const res =  verify(token, SECRET_KEY)
-   if(!res){
+    const res = verify(token, SECRET_KEY)
+    if (!res) {
       throw new UnauthorizedException('Invalid token')
-   }
-   const payload :any=  decode(token)
-   const user = await this.userService.findOneBy({id: payload.id})
-   if(user){
-     return user
-   }
+    }
+    const payload: any = decode(token)
+    const user = await this.userService.findOneBy({ id: payload.id })
+    if (user) {
+      return user
+    }
     throw new UnauthorizedException('Invalid token')
   }
   constructor(private userService: DeveloperService) { }
@@ -60,7 +60,8 @@ export class AuthenticationService {
     const emailToken = `${user.id}-${user._id}-${Date.now()}`;
     const emailTokenHash = this.createInputHash(emailToken)
     await this.userService.update(user.id, { emailToken: emailTokenHash })
-    await sendEmail(email, 'Verify your email', '<a href="http://localhost:3001/authentication/verifyEmail?token=' + emailTokenHash + '">Verify</a>')
+    await sendEmail(email, 'Verify your email', 
+    `<a href="${process.env.PUBLIC_API_URL}/authentication/verifyEmail?token=${emailTokenHash}">Click here to verify your email</a>`)
     return { id: user.id, isEmailVerified: user.isEmailVerified }
   }
 
@@ -71,21 +72,21 @@ export class AuthenticationService {
     }
     await this.userService.update(user.id, { isEmailVerified: true })
     return {
-      url: `http://localhost:3000/login`
+      url: `${process.env.PUBLIC_APP_URL}/auth/login`
     }
   }
-  async googleLogin(req) {
-    if (!req.user) {
-      return 'No user from google'
-    }
-    const user = req.user;
-    let dbUser: any = await this.userService.findByEmail(user.email)
-    if (!dbUser) {
-      dbUser = await this.userService.create(user)
-    }
+  // async googleLogin(req) {
+  //   if (!req.user) {
+  //     return 'No user from google'
+  //   }
+  //   const user = req.user;
+  //   let dbUser: any = await this.userService.findByEmail(user.email)
+  //   if (!dbUser) {
+  //     dbUser = await this.userService.create(user)
+  //   }
 
-    return { url: `http://localhost:3000/auth/redirect?accessToken=${req.user.accessToken}&userId:${dbUser.id}` }
-  }
+  //   return { url: `http://localhost:3000/auth/redirect?accessToken=${req.user.accessToken}&userId:${dbUser.id}` }
+  // }
 
   createInputHash(input: string) {
     return createHash('sha256').update(input).digest('hex');
