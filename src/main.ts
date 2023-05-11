9d6b0a022acd9f38fe67362ec5426d6a20cdccc9
bhavanad05 <bhavana.nd05@gmail.com>
2023-05-11 14:32:11 +0530

    changes for dashboad screen

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import  cors from 'cors'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.use(cors())
   await app.listen(3001);
 }
 bootstrap();
