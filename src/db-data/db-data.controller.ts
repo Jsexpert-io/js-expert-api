ba1ecf97c05256aceeba47c86536dd0606c088fa
bhavanad05 <bhavana.nd05@gmail.com>
2023-10-12 19:51:57 +0530

    changes for adding store and persistance

--- src/db-data/db-data.controller.ts
+++ src/db-data/db-data.controller.ts
@@ -1,32 +1,85 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Query } from '@nestjs/common';
 import { DbDataService } from './db-data.service';
 import { CreateDbDatumDto } from './dto/create-db-datum.dto';
 import { UpdateDbDatumDto } from './dto/update-db-datum.dto';
 import { PorjectGuard } from 'src/auth/project.guard';
 import { ApiTags } from '@nestjs/swagger';
 
-@Controller('db-data')
-@ApiTags('db-data')
+@Controller('server-data')
+@ApiTags('server-data')
 
 @UseGuards(PorjectGuard)
-
 export class DbDataController {
-  constructor(private readonly dbDataService: DbDataService) {}
+  constructor(private readonly serverDataService: DbDataService) { }
 
   @Post()
-  create(@Req() req:any, @Body() createServerDatumDto: CreateDbDatumDto) {
-    return this.dbDataService.create(createServerDatumDto,req.project._id);
+  create(@Req() req: any, @Body() createServerDatumDto: CreateDbDatumDto) {
+    return this.serverDataService.create(createServerDatumDto, req.project._id);
   }
 
+  // https://api.jsexpert.io/server-data?sortBy=&orderBy=&page=1&limit=10&search=
   @Get()
-  findAll(@Req() req:any) {
-    return this.dbDataService.findAllByProject(req.project._id);
+  findAll(@Req() req: any,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('sortBy') sortBy: string = 'createdAt',
+    @Query('orderBy') orderBy: string = 'desc',
+    @Query('search') search: string = '',
+  ) {
+    return this.serverDataService.findAllByProject(req?.project?._id,
+      page, limit, sortBy, orderBy, search
+    );
   }
 
- 
+  
+  @Get('findByEndpoint')
+  findByEndpoint(@Req() req: any,
+    @Query('endpoint') endpoint: string,
+  ) {
+    return this.serverDataService.findByEndpoint(
+      req?.project?._id,
+      endpoint
+    );
+  }
+
+  @Get('getRequestDistribution')
+  getRequestDistribution(@Req() req: any
+  ) {
+    return this.serverDataService.getRequestDistribution(
+      req?.project?._id
+    );
+  }
+
+  @Get('getRequestDurationDistribution')
+  getRequestDurationDistribution(@Req() req: any
+  ) {
+    return this.serverDataService.getRequestDurationDistribution(
+      req?.project?._id
+    );
+  }
+  @Get('getMemoryUsageTrend')
+  getMemoryUsageTrend(@Req() req: any,
+    @Query('startDate') startDate: Date,
+    @Query('endDate') endDate: Date = new Date(),
+
+  ) {
+    if(!startDate) startDate = new Date(new Date().setDate(new Date().getDate() - 30))
+
+    console.log(startDate,endDate)
+    return this.serverDataService.getMemoryUsageTrend(
+      req?.project?._id,startDate,endDate
+    );
+  }
+  @Get('errorStatusCodeDitribution')
+  errorStatusCodeDitribution(@Req() req: any
+  ) {
+    return this.serverDataService.errorStatusCodeDitribution(
+      req?.project?._id
+    );
+  }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.dbDataService.remove(id);
+    return this.serverDataService.remove(id);
   }
 }
--- src/db-data/db-data.service.ts
+++ src/db-data/db-data.service.ts
@@ -1,29 +1,227 @@
 import { Injectable } from '@nestjs/common';
-
-import { InjectModel } from '@nestjs/mongoose';
-
-import { Model } from 'mongoose';
-import { DbDataDocument } from './entities/db-datum.entity';
-
 import { CreateDbDatumDto } from './dto/create-db-datum.dto';
+import { UpdateDbDatumDto } from './dto/update-db-datum.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { DbDataDocument } from './entities/db-datum.entity';
+import { Model, PaginateModel } from 'mongoose';
 
 @Injectable()
 export class DbDataService {
 
+  findByEndpoint(_id: any, endpoint: string) {
+    return this.dbDataRepository.find({
+      project: _id,
+      'data.collectionName': endpoint
+    }).select(['data', 'createdAt'])
+
+  }
+
+  findByOperation(_id: any, endpoint: string) {
+    return this.dbDataRepository.find({
+      project: _id,
+      'data.operationType': endpoint
+    }).select(['data', 'createdAt'])
+
+  }
+
+
+  getRequestDistribution(_id: any) {
+    return this.dbDataRepository.aggregate([
+      {
+        $group: {
+          _id: null,
+          durations: { $push: '$data.durationInMilliseconds' },
+          successfulRequests: {
+            $sum: {
+              $cond: [
+                {
+                  $in: ['$data.responseObject.status', [200, 201, 202, 203, 204, 205, 206, 207, 208, 226]]
+                },
+                1,
+                0
+              ]
+            }
+          },
+          failedRequests: {
+            $sum: {
+              $cond: [
+                {
+                  $in: ['$data.responseObject.status',
+                    [400, 401, 402, 403,
+                      404, 405, 406, 407, 408, 409, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]]
+                },
+                1,
+                0
+              ]
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          durations: 1
+        }
+      }
+    ])
+  }
+
+  getRequestDurationDistribution(_id: any) {
+    console.log('getRequestDurationDistribution');
+    return this.dbDataRepository.aggregate([
+      {
+        $group: {
+          _id: '$data.requestObject.method',
+          count: { $sum: 1 },
+          successfulRequests: {
+            $sum: {
+              $cond: [
+                {
+                  $in: ['$data.responseObject.status', [200, 201, 202, 203, 204, 205, 206, 207, 208, 226]]
+                },
+                1,
+                0
+              ]
+            }
+          },
+          failedRequests: {
+            $sum: {
+              $cond: [
+                {
+                  $in: ['$data.responseObject.status',
+                    [400, 401, 402, 403,
+                      404, 405, 406, 407, 408, 409, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]]
+                },
+                1,
+                0
+              ]
+            }
+          }
+        }
+      },
+        // {
+        //   $project: {
+        //     _id: 0,
+        //     method: '$_id',
+        //     count: 1
+        //   }
+        // }
+    ])
+  }
+  getMemoryUsageTrend(_id: any, startDate: Date, endDate: Date) {
+    return this.dbDataRepository.aggregate([
+      
+      {
+        $group: {
+          _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+          avgMemoryUsage: { $avg: '$data.memoryUsage' }
+        }
+      },
+      {
+        $sort: { _id: 1 }
+      },
+      {
+        $project: {
+          _id: 0,
+          date: '$_id',
+          avgMemoryUsage: 1
+        }
+      }
+    ])
+  }
+  errorStatusCodeDitribution(_id: any) {
+    return this.dbDataRepository.aggregate([
+      {
+        $group: {
+          _id: '$data.responseObject.status',
+          count: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          statusCode: '$_id',
+          count: 1
+        }
+      }
+    ])
+  }
+
   constructor(
     @InjectModel('dbdata')
-    private dbDataRepository: Model<DbDataDocument>,
+    private dbDataRepository: PaginateModel<DbDataDocument>,
   ) { }
-  create(createServerDatumDto: CreateDbDatumDto,projectId:string) {
+  create(createServerDatumDto: CreateDbDatumDto, projectId: string) {
     return this.dbDataRepository.create({
-      ...createServerDatumDto,project:projectId
+      ...createServerDatumDto, project: projectId
     })
   }
 
-  findAllByProject(_id: any) {
-    return this.dbDataRepository.find({project:_id})
+  findAllByProject(projectId: string, page: number, limit: number, sortBy: string, orderBy: string, search: string) {
+
+    const matchQuery = {
+      project: String(projectId),
+    }
+    if (search) {
+      matchQuery['data.collectionName'] = { $regex: search, $options: 'i' }
+    }
+    console.log(JSON.stringify(matchQuery));
+
+    return this.dbDataRepository.aggregate([
+      {
+        $match: matchQuery
+      },
+      {
+        $group: {
+          _id: {
+            endpoint: '$data.collectionName',
+            method: '$data.operationType'
+          },
+          count: { $sum: 1 },
+          avgDuration: { $avg: '$data.durationInMilliseconds' },
+
+          latestRequestDate: { $max: '$createdAt' },
+    
+          
+        }
+      },
+      {
+        $sort: {
+          [sortBy || "createdAt"]: orderBy === 'asc' ? 1 : -1
+        }
+      },
+      {
+        $facet: {
+          metadata: [{ $count: 'total' }],
+          paginatedData: [{ $skip: (page - 1) * Number(limit) }, { $limit: Number(limit) }]
+        }
+      }
+    ])
+      .then((result) => {
+        const metadata = result[0].metadata[0];
+        const paginatedData = result[0].paginatedData;
+        const total = metadata ? metadata.total : 0;
+        return {
+          total,
+          paginatedData
+        }
+        console.log('Total documents:', total);
+        console.log('Paginated data:', paginatedData);
+      })
+    // return this.dbDataRepository.aggregate([
+    //   // {
+    //   //   $match: { project: _id }
+    //   // },
+
+    // ])
+    // return this.dbDataRepository.paginate({project:_id},
+    //   {
+    //     page: pagenumber,
+    //     limit,
+    //     select: ['data','createdAt']
+    //   })
   }
-  
+
   remove(id: string) {
     return this.dbDataRepository.deleteOne({ id })
   }
