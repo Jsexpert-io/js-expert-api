c64a0e0b37451a9acd484f05600b235477340406
bhavanad05 <bhavana.nd05@gmail.com>
2024-02-10 18:14:55 +0530

    Initial changeS

--- src/server-data/v1/metric/metric.module.ts
+++ src/server-data/v1/metric/metric.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MetricService } from './metric.service';
+import { ProjectModule } from 'src/project/project.module';
 import { MetricController } from './metric.controller';
+import { MetricService } from './metric.service';
 
 @Module({
+  imports: [
+    ProjectModule
+  ],
   controllers: [MetricController],
   providers: [MetricService]
 })
-export class MetricModule {}
+export class MetricModule { }
