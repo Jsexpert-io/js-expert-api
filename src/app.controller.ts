5a97aa201ed5b187bf3ef00cfefac41a98a848ef
bhavanad05 <bhavana.nd05@gmail.com>
2024-02-18 08:57:23 +0530

    Initial changeS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from '@nestjs/common';
-import { prisma } from './Utils/DbService';
 import { AppService } from './app.service';
 
 
@@ -8,8 +7,6 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
   @Get()
   async getHello() {
-    console.log('getHello')
-    await prisma.traceSpan.deleteMany()
     return this.appService.getHello();
   }
 }
